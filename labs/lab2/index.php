4f82011d450c5d8443fbcc537078e6627edfa9d3
Mali King <markiepeanut@gmail.com>
2017-09-05 17:45:48 +0000

    Adding lab 2

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -2,8 +2,9 @@
 <html>
     <head>
         <title> 777 Slot Machine </title>
+        <meta charset="utf-8"/>
     </head>
     <body>
-
+        <img src="img/orange.png" alt="orange" title="Orange" width="70" />
     </body>
 </html>
