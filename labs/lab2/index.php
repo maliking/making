1f21bcc95e2c944179930e1b680e91b3857169f2
Mali King <markiepeanut@gmail.com>
2017-09-05 18:02:05 +0000

    Adding lab 2

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -7,9 +7,11 @@
     <body>
         <?php
             $symbol = "grapes";
-            <img src="img/$symbol.png" alt=$symbol title=$symbol width="70" />
-            <img src="img/$symbol.png" alt=$symbol title=$symbol width="70" />
-            <img src="img/$symbol.png" alt=$symbol title=$symbol width="70" />
+            $randomNumber = rand(0,5);
+            echo $randomNumber;
+            echo "<img src="img/$symbol.png" alt=$symbol title=$symbol width="70"" />
+            echo "<img src="img/$symbol.png" alt=$symbol title=$symbol width="70"" />
+            echo "<img src="img/$symbol.png" alt=$symbol title=$symbol width="70"" />
         ?>
 
         <img src="img/orange.png" alt="orange" title="Orange" width="70" />
