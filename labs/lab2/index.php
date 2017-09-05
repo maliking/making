8f32b86851b1bfc53296d48f916f97c2ab08533d
Mali King <markiepeanut@gmail.com>
2017-09-05 17:59:45 +0000

    Adding lab 2

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -5,6 +5,14 @@
         <meta charset="utf-8"/>
     </head>
     <body>
+        <?php
+            $symbol = "grapes";
+            <img src="img/$symbol.png" alt=$symbol title=$symbol width="70" />
+            <img src="img/$symbol.png" alt=$symbol title=$symbol width="70" />
+            <img src="img/$symbol.png" alt=$symbol title=$symbol width="70" />
+        ?>
+
+        <img src="img/orange.png" alt="orange" title="Orange" width="70" />
         <img src="img/orange.png" alt="orange" title="Orange" width="70" />
     </body>
 </html>
