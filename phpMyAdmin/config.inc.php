cde07bf78e9025a2f642ee36351a70c8b2bfa3fe
Mali King <markiepeanut@gmail.com>
2017-10-14 21:18:44 +0000

    Adding lab5

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -21,9 +21,19 @@ $cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
  */
 $i = 0;
 
+
 /**
  * First server
  */
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+
 $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'config';
