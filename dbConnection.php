e9dd14bc0f91a4a8b30012f2d6cd5aa200ac5156
Mali King <markiepeanut@gmail.com>
2017-10-17 19:16:36 +0000

    Test

--- dbConnection.php
+++ dbConnection.php
@@ -1,23 +1,23 @@
 <?php
 function getDatabaseConnection($dbname='quotes') {
-    // cloud9
-    $host = 'localhost';
-    $dbname = 'quotes';
-    $username = 'maliking';
-    $password = '';
+    // // cloud9
+    // $host = 'localhost';
+    // $dbname = 'quotes';
+    // $username = 'maliking';
+    // $password = '';
 
-    // Heroku Connection
-    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) { // if the server starts with making-cst352.herokuapp.com, run
+    // // Heroku Connection
+    // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) { // if the server starts with making-cst352.herokuapp.com, run
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
         $server = $url["us-cdbr-iron-east-05.cleardb.net"];
         $username = $url["b7506be644b2df"];
         $password = $url["b8b7b36d"];
         $db = substr($url["heroku_ead1f57be8a54a6"], 1);
-    }
+    // }
 
     try {
-    $dbConn = new PDO("mysql:host=$server;dbname=$db", $username, $password);
+    $conn = new PDO("mysql:host=$server;dbname=$db", $username, $password);
     foreach($dbh->query('SELECT * from q_quote') as $row) {
         print_r($row);
     }
@@ -26,8 +26,8 @@ function getDatabaseConnection($dbname='quotes') {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    return $dbConn;
+    return $conn;
 }
 ?>
