9a9e8acdff340a859192bc2d9873e415fedf8e42
Mali King <markiepeanut@gmail.com>
2017-10-17 18:59:40 +0000

    Test

--- dbConnection.php
+++ dbConnection.php
@@ -9,13 +9,14 @@ function getDatabaseConnection($dbname='quotes') {
     // Heroku Connection
     if  (strpos($_SERVER['HTTP_HOST'], 'making-cst352') !== false) { // if the server starts with making-cst352.herokuapp.com, run
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["us-cdbr-iron-east-05.cleardb.net"];
-        $dbname = substr($url["heroku_ead1f57be8a54a6"], 1);
+
+        $server = $url["us-cdbr-iron-east-05.cleardb.net"];
         $username = $url["b7506be644b2df"];
         $password = $url["b8b7b36d"];
+        $db = substr($url["heroku_ead1f57be8a54a6"], 1);
     }
 
-    $dbConn = new PDO("mysql: host=$host;dbname=$dbname", $username, $password);
+    $dbConn = new PDO("mysql:host=$server;dbname=$db", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     return $dbConn;
