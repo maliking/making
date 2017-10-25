0471459806494563545f4ad961df927dfbb44e58
Mali King <markiepeanut@gmail.com>
2017-10-25 18:05:31 +0000

    Adding lab6 & 7

--- index.php
+++ index.php
@@ -5,5 +5,18 @@
     </head>
     <body>
         <h1>Mali King's CST-352</h1>
+        <h3>Homework</h3>
+        <ul>
+        <?php
+        for ($i = 0; $i < 10; $i++) {
+            echo "<li>";
+            echo "<a href='https://making-cst352.herokuapp.com/hw/hw" . $i . "/index.php";
+            echo "Homework " . $i . "</a>";
+            echo "</li>";
+        }
+        ?>
+        </ul>
+        <h3>Labs</h3>
+        <h3>Exams</h3>
     </body>
 </html>
