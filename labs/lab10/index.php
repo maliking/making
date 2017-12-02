e6785b99b1636a18f72ba3f0d8d49f122cc7c0fb
Mali King <markiepeanut@gmail.com>
2017-12-02 20:58:32 +0000

    Adding lab10

--- labs/lab10/index.php
+++ labs/lab10/index.php
@@ -1,57 +1,64 @@
 <?php
+    function displayImages() {
+        $folder = "gallery/";
+        $results = scandir($folder, 1);
 
-function checkSize() {
-    $size = filesize("gallery/" . $file);
-
-    if ($size > 1048576) {
-        echo "image is a good size";
-    } else {
-        echo "image is too big";
-    }
-}
-function displayImages() {
-    if (isset($_POST['submitForm'])) { //checks whether the form has been submitted
-        move_uploaded_file($_FILES['myFile']['tmp_name'], "gallery/" . $_FILES['myFile']['name'] );
-        $files = scandir("gallery/", 1);
-
-        for ($i = 0; $i < count($files)-2; $i++) {
-          echo "<a href='gallery/" . $files[$i] . "'><img src='gallery/".  $files[$i] . "' width='35' ></a> <br />";
+        foreach ($results as $result) {
+            if ($results === '.' || $results === '..') {
+                continue;
+            }
+            if (is_file($folder . $result)) {
+                echo
+                '<div class="col-md-1">
+                    <div class="thumbnail">
+                        <a href=' . $folder . $result . '><img src="' . $folder . $result.'" alt="image"></a>
+                    </div>
+                </div>';
+            }
         }
-    } //endIf
-}
-
+    }
 ?>
-
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Lab 10: File Upload </title>
+        <title> Lab 10: File Upload </title>
         <meta charset="utf-8" />
         <script src="../../bootstrap/js/jquery.min.js"></script>
         <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
         <script src="../../bootstrap/js/bootstrap.min.js"></script>
+        <script src="js/bootstrap-filestyle.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Bungee+Inline|Lato" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-6 col-md-offset-3">
+            <h1> <img id="camera" src='img/camera.png'>Photo Gallery </h1>
+            <h4> Upload your images here! </h4>
+            <form action="upload.php" method="POST" enctype="multipart/form-data">
+                Upload file:
+                <input type="file" name="image"/>
+                <input class="btn btn-success btn-block" type="submit" name="submitForm" value="Upload!" />
+            </form>
+            </div>
+        </div>
+        <div class="row">
+        <?=displayImages()?>
+        </div>
+    </div>
 
-    <h1> Photo Gallery </h1>
+    <div class="navbar navbar-default navbar-fixed-bottom">
+        <div class="container">
+            <p class="navbar-text">© 2017 - Mali King </p>
+        </div>
+    </div>
 
-    <form action="upload.php" method="POST" enctype="multipart/form-data">
-
-        Upload file:
-
-        <input type="file" name="myFile"/>
-
-        <input type="submit" name="submitForm" value="Upload!" />
-
-    </form>
-
-    <br />
-
-    <?=displayImages()?>
-
-
+    <script>
+        $(":file").filestyle();
+    </script>
     </body>
 </html>
--- labs/lab10/upload.php
+++ labs/lab10/upload.php
@@ -1,28 +1,25 @@
 <?php
-$target_dir = "gallery/";
-$target_file = $target_dir . basename($_FILES["myFile"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    if (isset($_FILES['image'])) {
+        $errors = array();
+        $file_name = $_FILES['image']['name'];
+        $file_size = $_FILES['image']['size'];
+        $file_tmp = $_FILES['image']['tmp_name'];
+        $file_type = $_FILES['image']['type'];
 
-if(isset($_POST["submit"])) {
-$check = getimagesize($_FILES["myFile"]["tmp_name"]);
-}
+        if ($file_size > 1048576) {
+            echo "<h3>File size must be under 1MB<h3>";
+            $errors[] = '<pre>File size must be under 1MB</pre>';
+        }
 
- // Check file size
-if ($_FILES["myFile"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["myFile"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["myFile"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
+        if (empty($errors) == true) {
+            move_uploaded_file($file_tmp, "gallery/" . $file_name);
+            echo "Success";
+        }
+        else {
+            echo "<h4>Your file has not been uploaded. Please try again.</h4>";
+            echo "<pre>";
+            print_r($errors);
+            echo "</pre>";
+        }
     }
-}
 ?>
